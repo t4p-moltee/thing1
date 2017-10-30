6580e947223f327b732330f453fbd2f92f35d585
Malte Wittkugel <malte@things4people.com>
2017-10-30 23:33:55 +0100

    Projektdateien hinzufügen.

---
+++ T4P/T4P/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace T4P
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
